d808fd67ac921c821aa13c50a2d42affb347cb67
Anton <shredor@icloud.com>
2019-08-02 14:24:46 +0300

    Add files via upload

---
+++ timezone-tester-2/precache-manifest.14bbad9aa521e4d4bd60db621edd3f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d517867e83ab07bc4b78bec98cd2844a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39264fa52261f618e411",
+    "url": "/timezone-tester/static/css/main.4e551f47.chunk.css"
+  },
+  {
+    "revision": "f208fa859a813d295e6f",
+    "url": "/timezone-tester/static/js/2.3c49f788.chunk.js"
+  },
+  {
+    "revision": "39264fa52261f618e411",
+    "url": "/timezone-tester/static/js/main.39d98c4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/timezone-tester/static/js/runtime~main.a8a9905a.js"
+  }
+]);
